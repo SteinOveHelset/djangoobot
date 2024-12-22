3140aaffea2f9ddae3ec044927182c480fefab26
Stein Ove Helset <steinovehelset@gmail.com>
2024-12-22 07:58:23 +0100

    pr testing

--- djangoobot/urls.py
+++ djangoobot/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def index(request):
+    return HttpResponse('This is just a test')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
